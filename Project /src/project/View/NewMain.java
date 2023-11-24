05f98e59c51376c0e7a78129da8282b46c132e55
Marco Bove <149075171+MarcoBove@users.noreply.github.com>
2023-11-24 14:25:18 +0100

    Update NewMain.java

--- Project /src/project/View/NewMain.java
+++ Project /src/project/View/NewMain.java
@@ -15,6 +15,8 @@ public class NewMain {
     /**
      * @param args the command line arguments
      */
+
+    //yoo
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("primo");
